1a171b1e254b6f358391b3e5c5d68c8514d71ab8
Tyler Collier <tyler.collier+github@devmounta.in>
2018-02-21 00:43:52 -0700

    Complete steps before adding bcrypt

--- server/index.js
+++ server/index.js
@@ -16,15 +16,37 @@ app.use(session({
 app.use(express.static(`${__dirname}/../build`));
 
 app.post('/register', (req, res) => {
-  // Add code here
+  const db = app.get('db');
+  const { username, password } = req.body;
+  db.create_user([username, password]).then(() => {
+    req.session.user = { username };
+    res.json({ user: req.session.user })
+  }).catch(error => {
+    console.log('error', error);
+    res.status(500).json({ message: 'Something bad happened! '})
+  });
 });
 
 app.post('/login', (req, res) => {
-  // Add code here
+  const db = app.get('db');
+  const { username, password } = req.body;
+  db.find_user([username]).then(users => {
+    if (users.length) {
+      if (users[0].password === password) {
+        req.session.user = { username: users[0].username };
+        res.json({ user: req.session.user });
+      } else {
+        res.status(403).json({ message: 'Wrong password' })
+      }
+    } else {
+      res.status(403).json({ message: "That user is not registered" })
+    }
+  });
 });
 
 app.post('/logout', (req, res) => {
-  // Add code here
+  req.session.destroy();
+  res.status(200).send();
 });
 
 function ensureLoggedIn(req, res, next) {
